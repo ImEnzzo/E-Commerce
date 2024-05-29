cdf079b06c97937cd174106317d826fd25074ecf
ImEnzzo <lorenzosanchezjr0309@gmail.com>
2024-05-28 19:31:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' elemnt={<ShopCategory category="mens"/>}/>
+      <Route path='/womens' elemnt={<ShopCategory category="womens"/>}/>
+      <Route path='/kids' elemnt={<ShopCategory category="kids"/>}/>
+      <Route path="product" element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignp/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
